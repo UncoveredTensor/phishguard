363801aec9d4acb4523791487e79eb82e7f96ac9
ceyhuncakir <ceyhuncakir@live.nl>
2023-06-29 11:59:46 +0200

    fix: added new load model code
    added new load model code

--- src/api/endpoint.py
+++ src/api/endpoint.py
@@ -3,28 +3,32 @@ import xgboost as xgb
 from fastapi import FastAPI
 from src.features import Features
 import pandas as pd
+from mlflow.tracking import MlflowClient
+import mlflow
 
 app = FastAPI()
 
 @app.on_event("startup")
 def load_model():
+    mlflow.set_tracking_uri("http://127.0.0.1:5000") # replace with your mlflow service URL
+    client = MlflowClient()
 
-    runs = mlflow.search_runs(experiment_ids="phishguard")
+    # Replace 'YourExperimentID' with your actual experiment id and 'accuracy' with the metric you are interested in.
+    experiment_id = "phishguard"
+    runs = client.search_runs(experiment_ids=experiment_id,
+                              filter_string="",
+                              run_view_type=mlflow.entities.ViewType.ACTIVE_ONLY,
+                              max_results=1000,
+                              order_by=["metrics.accuracy DESC"])
 
-    print(runs)
+    # Get the top run (with the highest accuracy)
+    top_run = runs[0]
 
-    # Sort the runs by accuracy in descending order
-    sorted_runs = runs.sort_values(by="metrics.accuracy", ascending=False)
-
-    # Get the best run and its corresponding run ID
-    best_run_id = sorted_runs.iloc[0]["run_id"]
-
-    model = mlflow.xgboost.load_model(f"runs:/{best_run_id}/xgb_model")
+    model = mlflow.xgboost.load_model(f"runs:/{top_run.info.run_id}/xgb_model")
     top_features = model.get_xgb_params()['top_features']
-    scaler = mlflow.sklearn.load_model(f"runs:/{best_run_id}/min_max_scaler")
+    scaler = mlflow.sklearn.load_model(f"runs:/{top_run.info.run_id}/min_max_scaler")
 
-    model_uri = "runs:/<run_id>/model"
-    app.model = mlflow.xgboost.load_model(model_uri)
+    app.model = model
     app.scaler = scaler
     app.top_features = top_features
 
