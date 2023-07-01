2140e7cbf5ff7ad002d92e2150b6b852b0c95ac9
ceyhuncakir <ceyhuncakir@live.nl>
2023-07-01 10:14:10 +0200

    feature: added docstrings and added a response body
    added the following features:
    
    - added docstrings to the functions
    - added a response body

--- src/api/endpoint.py
+++ src/api/endpoint.py
@@ -19,8 +19,25 @@ class Domain(BaseModel):
 class DomainsRequest(BaseModel):
     domains: List[Domain]
 
+class DomainsRequest(BaseModel):
+    domains: list
+
+class PredictionResponse(BaseModel):
+    url: str
+    prediction: str
+
 @app.on_event("startup")
 def load_model():
+
+    """this function is used to load the model and the scaler from the MLflow run.
+
+    Args:
+        None
+    
+    Returns:
+        None
+    """
+        
     runs = mlflow.search_runs(experiment_ids="654885389741096205")
 
     # Sort the runs by accuracy in descending order
@@ -29,26 +46,50 @@ def load_model():
     # Get the best run and its corresponding run ID
     best_run_id = sorted_runs.iloc[0]["run_id"]
 
+    # Load in the models
     model = mlflow.xgboost.load_model(f"{os.getcwd()}/mlruns/654885389741096205/{best_run_id}/artifacts/xgb_model")
     top_features = model.get_xgb_params()['top_features']
     scaler = mlflow.sklearn.load_model(f"{os.getcwd()}/mlruns/654885389741096205/{best_run_id}/artifacts/min_max_scaler")
 
+    # Assign the models to the app
     app.model = model
     app.scaler = scaler
     app.top_features = top_features
 
 @app.post("/predict")
-def predict(data: DomainsRequest):
+def predict(data: DomainsRequest) -> List[PredictionResponse]:
 
+    """This function is used to predict the class of a url or a list of urls.
+
+    Args:
+        data (Union[str, str]): The url or the path to the file that contains the list of urls.
+    
+    Returns:
+        List[PredictionResponse]: A list of PredictionResponse objects.
+    """
+
+    # Getting the post data
     domains = data.domains
 
+    # Getting the urls from the post data
     domains = [domain.name for domain in domains]
 
+    # Creating a dataframe with the urls
     df = pd.DataFrame(domains, columns=['url'])
 
+    # Extracting the features
     feature_extraction = Features()
     feature_results = df.apply(lambda row: feature_extraction.extract_features(url=row.iloc[0], source_data="src/data/dataset.csv", top_features=app.top_features), axis=1)
+
+    # Concatenating the features to the dataframe
     df = pd.concat([df, feature_results.apply(pd.Series)], axis=1)
     df.iloc[:, 1:] = app.scaler.transform(df.iloc[:, 1:])
+
+    # Predicting the class of the urls  
     df['prediction'] = df.apply(lambda row: app.model.predict(row[1:].values.reshape(1, -1))[0], axis=1)
-    print(df)
+
+    # Creating the response
+    response = [PredictionResponse(url=row['url'], prediction=row['prediction']) for idx, row in df.iterrows()]
+
+    return response
+    
