6de9dd2bfa014c34d02d3e5052c0803ce6f7636c
ceyhuncakir <ceyhuncakir@live.nl>
2023-07-08 18:47:06 +0200

    fix: fixed the same bug in api

--- src/api/endpoint.py
+++ src/api/endpoint.py
@@ -42,16 +42,17 @@ def load_model():
 
     # Get the best run and its corresponding run ID
     best_run_id = sorted_runs.iloc[0]["run_id"]
+    best_run = mlflow.get_run(best_run_id)
+    top_features = best_run.data.params['top_features']
 
     # Load in the models
     model = mlflow.xgboost.load_model(f"{os.getcwd()}/mlruns/654885389741096205/{best_run_id}/artifacts/xgb_model")
-    top_features = model.get_xgb_params()['top_features']
     scaler = mlflow.sklearn.load_model(f"{os.getcwd()}/mlruns/654885389741096205/{best_run_id}/artifacts/min_max_scaler")
 
     # Assign the models to the app
     app.model = model
     app.scaler = scaler
-    app.top_features = top_features
+    app.top_features = int(top_features)
 
 @app.post("/predict")
 def predict(data: DomainsRequest) -> List[PredictionResponse]:
